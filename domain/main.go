cc39966428a1cc2bee11087c510d696b05b96610
Alejandro Tomasella <aletomasella09@gmail.com>
2023-10-29 20:33:52 -0300

    feat: First rendering
    ]

--- domain/main.go
+++ domain/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
@@ -11,18 +12,18 @@ func main() {
 
 func inicializeServer() {
 
-	app := fiber.New()
+	engine := html.New("../templates", ".html")
+
+	app := fiber.New(fiber.Config{Views: engine})
+
+	app.Static("/", "../static")
 
 	app.Use(cors.New(cors.ConfigDefault))
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Redirect("/api")
+		return c.Render("index", fiber.Map{})
 	})
 
-	app.Get("/api", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Listen(":3000")
+	app.Listen("localhost:3000")
 
 }
