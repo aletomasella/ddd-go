9d2cbf15ed7bb3b6934e6aa7895238c91058d1ab
Alejandro Tomasella <aletomasella09@gmail.com>
2023-10-29 19:57:38 -0300

    feat: Starting to work on a ToDo App build with HTMX

--- domain/main.go
+++ domain/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
 func main() {
+	inicializeServer()
+}
+
+func inicializeServer() {
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.ConfigDefault))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Redirect("/api")
+	})
+
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+
 }
