a552b6fed13f7e6dc7845b2e66ad43698c4a2faf
Alejandro Tomasella <aletomasella09@gmail.com>
2023-10-29 21:35:51 -0300

    feat: Adding hot reloading with air

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	inicializeServer()
+}
+
+func inicializeServer() {
+
+	engine := html.New("./templates", ".html")
+
+	app := fiber.New(fiber.Config{Views: engine})
+
+	app.Static("/static", "./static")
+
+	app.Use(cors.New(cors.ConfigDefault))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("Base", fiber.Map{})
+	})
+
+	app.Listen("localhost:3000")
+
+}
