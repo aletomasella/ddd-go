6ac5ca680d474258cbff0552ee792a74ae110ce8
Alejandro Tomasella <aletomasella09@gmail.com>
2023-11-01 20:27:21 -0300

    refactor: Hot reloading not working as expected

--- src/htmx.js
+++ src/htmx.js
@@ -3,28 +3,20 @@ import htmx from "htmx.org";
 // Assinging htmx to window object
 window.htmx = htmx;
 
-(function () {
-  let flag = false;
+// async function checkToHotReload() {
+//   try {
+//     const response = await fetch("/reload");
+//     const data = await response.json();
 
-  async function checkToHotReload() {
-    try {
-      const response = await fetch("/reload");
-      const data = await response.json();
+//     if (data.reload === "true") {
+//       window.location.reload();
+//     } else {
+//       window.reloadIntervalId && clearInterval(window.reloadIntervalId);
+//       window.reloadIntervalId = null;
+//     }
+//   } catch (error) {
+//     window.location.reload();
+//   }
+// }
 
-      if (data.reload === "true") {
-        window.location.reload();
-      } else {
-        flag = true;
-      }
-    } catch (error) {
-      window.location.reload();
-    }
-  }
-
-  const intervalId = setInterval(checkToHotReload, 1000);
-
-  if (flag) {
-    console.log("clearing interval");
-    intervalId = clearInterval(intervalId);
-  }
-})();
+// window.reloadIntervalId = setInterval(checkToHotReload, 1000);
